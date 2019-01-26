c6c80eb3351d707d58529b6c53980520836de257
Carlos Vazquez <crlsvzqz5@gmail.com>
2019-01-26 15:57:40 -0500

    mytest fix

--- mytest.c
+++ mytest.c
@@ -1,18 +1,25 @@
-#include <stdio.h>
+//#include <stdio.h>
 
 #define N 1000
 
-int main() {
-	// Without mem_barrier
+void without_mem_barrier() {
 	int i = 0;
 	int a[N];
 	for (; i<N; i++)
 		a[i]++;
+}
 
-	// With mem_barrier
-	i=0;
+void with_mem_barrier() {
+	int i=0;
+	int a[N];
 	for (; i<N; i++) {
-		asm (""::"m" (i));
+		//asm (""::"m" (i));
+		asm ("":::"memory");
 		a[i]++;
 	}
+}
+
+int main() {
+	without_mem_barrier();
+	with_mem_barrier();
 }
