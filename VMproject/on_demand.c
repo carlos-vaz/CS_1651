e271d8c7654473e44e744531d83673c0d6caabfb
Carlos Daniel Vazquez-Gomez Bertelli <Daniel@fq-wifi-10-215-177-193.wireless.pitt.edu>
2019-02-23 14:03:41 -0500

    derefed pml entry

--- VMproject/on_demand.c
+++ VMproject/on_demand.c
@@ -112,8 +112,8 @@ petmem_handle_pagefault(struct mem_map * map,
 	uintptr_t v_pml_dest = v_cr3 + pml_index;
 	printk("PML Table + PML Index (virtual) = %lx\n", v_pml_dest);
 	printk("Dereferencing...\n");
-	unsigned long pml_dest_data = *v_pml_dest;
-	printk("PML entry data = %lx \n", pml_dest_data);
+	//unsigned long pml_dest_data = *v_pml_dest;
+	//printk("PML entry data = %lx \n", pml_dest_data);
 
 
 	
