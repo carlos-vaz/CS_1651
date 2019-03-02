552401fd1afdbb31ee8c0f01141a555222d48d32
Carlos Daniel Vazquez-Gomez Bertelli <Daniel@mc-wifi-10-215-110-246.wireless.pitt.edu>
2019-03-02 12:17:32 -0500

    linked list free

--- VMproject/user/test.c
+++ VMproject/user/test.c
@@ -6,40 +6,61 @@
 
 
 int main(int argc, char ** argv) {
-    init_petmem();
+	init_petmem();
 
-    char * buf = NULL;
-    buf = pet_malloc(4096);
+	char * buf, *buf1, *buf2, *buf3, *buf4, *buf5;
+	buf = pet_malloc(4096);
+	pet_dump();
+	buf1 = pet_malloc(8192);
+	pet_dump();
+	buf3 = pet_malloc(4096);
+	pet_dump();
+	buf4 = pet_malloc(40960);
+	pet_dump();
+	buf5 = pet_malloc(234);
+	pet_dump();
 
-    printf("Allocated 1 page at %p\n", buf);
+	//printf("Allocated 1 page at %p\n", buf);
 
-    pet_dump();
-
-
-    buf[50] = 'H';
-    buf[51] = 'e';
-    buf[52] = 'l';
-    buf[53] = 'l';
-    buf[54] = 'o';
-    buf[55] = ' ';
-    buf[56] = 'W';
-    buf[57] = 'o';
-    buf[58] = 'r';
-    buf[59] = 'l';
-    buf[60] = 'd';
-    buf[61] = '!';
-    buf[62] = 0;
+	pet_dump();
 
 
-    printf("%s\n", (char *)(buf + 50));
+	buf[50] = 'H';
+	buf[51] = 'e';
+	buf[52] = 'l';
+	buf[53] = 'l';
+	buf[54] = 'o';
+	buf[55] = ' ';
+	buf[56] = 'W';
+	buf[57] = 'o';
+	buf[58] = 'r';
+	buf[59] = 'l';
+	buf[60] = 'd';
+	buf[61] = '!';
+	buf[62] = 0;
 
 
-    pet_free(buf);
-    pet_dump();
-/*
-    printf("%s\n", (char *)(buf + 50));
-*/ 
+	printf("%s\n", (char *)(buf + 50));
 
-    return 0;
+
+
+	pet_free(buf);
+	pet_dump();
+	pet_free(buf1);
+	pet_dump();
+	pet_free(buf2);
+	pet_dump();
+	pet_free(buf3);
+	pet_dump();
+	pet_free(buf4);
+	pet_dump();
+	pet_free(buf5);
+	pet_dump();
+
+	/*
+	printf("%s\n", (char *)(buf + 50));
+	*/ 
+
+	return 0;
 
 }
