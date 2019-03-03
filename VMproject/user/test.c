d095dc49012bd4c6a092bbd711b17d6599c9d6b8
Carlos Daniel Vazquez-Gomez Bertelli <Daniel@fq-wifi-10-215-178-165.wireless.pitt.edu>
2019-03-03 17:17:30 -0500

    free table pages

--- VMproject/user/test.c
+++ VMproject/user/test.c
@@ -11,7 +11,7 @@ int main(int argc, char ** argv) {
 	char * buf, *buf1, *buf2, *buf3, *buf4, *buf5;
 	buf = pet_malloc(4096);
 	pet_dump();
-/*	buf1 = pet_malloc(8192);
+	buf1 = pet_malloc(8192);
 	pet_dump();
 	buf2 = pet_malloc(2176353);
 	pet_dump();
@@ -21,8 +21,6 @@ int main(int argc, char ** argv) {
 	pet_dump();
 	buf5 = pet_malloc(234);
 	pet_dump();
-*/
-	//printf("Allocated 1 page at %p\n", buf);
 
 
 	buf[50] = 'H';
@@ -50,7 +48,7 @@ int main(int argc, char ** argv) {
 	printf("Kernel space access from userland: %c\n", b_k[0]); // SHould give 'lack of permission' sig code on Seg Fault
 */								   // No! Sig code actually tells us 'not present'
 	
-/*	pet_free(buf1);
+	pet_free(buf1);
 	pet_dump();
 	pet_free(buf2);
 	pet_dump();
@@ -60,7 +58,7 @@ int main(int argc, char ** argv) {
 	pet_dump();
 	pet_free(buf5);
 	pet_dump();
-*/
+
 /*	char * b = malloc(20);
 	b[0] = 'u';
 	b[20] = 'u'; 
